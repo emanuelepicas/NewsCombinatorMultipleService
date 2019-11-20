3cdf817a0d21f29b1bccd03ff29966bdbaa5bd32
Emanuele Picariello <emanuele.picariello@sourcesense.com>
2019-11-20 15:46:08 +0100

    Initial Commit

---
+++ src/main/java/com/sourcesense/emanuelepicariello/demo/NewsControllerMultipleServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sourcesense.emanuelepicariello.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewsControllerMultipleServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewsControllerMultipleServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sourcesense/emanuelepicariello/demo/NewsControllerMultipleServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sourcesense.emanuelepicariello.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewsControllerMultipleServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
